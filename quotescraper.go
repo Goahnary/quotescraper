3a1a6b327a0584682047a524dc208230cd2c1d91
Noah Gary <goahnary@gmail.com>
2019-06-25 00:14:15 -0400

    Not working favorably, but collecting all quotes. Milestone.

--- quotescraper.go
+++ quotescraper.go
@@ -17,34 +17,46 @@ func main () {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	filter := scanner.Text();
-	// See http://toscrape.com/ for more options!
-	res, err := http.Get("http://quotes.toscrape.com/")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer res.Body.Close()
-	if res.StatusCode != 200 {
-		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
-	}
-
-	// Load the HTML document
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	selector := "[itemprop='text']"
-
-	filteredResults := doc.Find(selector).FilterFunction(func(i int, s *goquery.Selection) bool {
-		return strings.Contains(s.Text(), filter);
-	});
 
 	fmt.Printf("\n");
 	fmt.Printf("Filtered Results:");
+	// Loop through to last page.
+	// I have already determined the last page for this site.
+	// TODO: detect 404 and stop loop when detected.
+	i := 1
+	for i < 11 {
+		url := fmt.Sprintf("http://quotes.toscrape.com/page/%v", i);
 
-	filteredResults.Each(func(i int, s *goquery.Selection){
-		fmt.Printf("\n");
-		fmt.Printf("%v. %v\n", i+1, s.Text());
-	});
+		// See http://toscrape.com/ for more options!
+		res, err := http.Get(url)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer res.Body.Close()
+		if res.StatusCode != 200 {
+			log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+		}
+
+		// Load the HTML document
+		doc, err := goquery.NewDocumentFromReader(res.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		selector := "[itemprop='text']"
+
+		// FIXME: Is this accumulative?
+		filteredResults := doc.Find(selector).FilterFunction(func(i int, s *goquery.Selection) bool {
+			return strings.Contains(s.Text(), filter);
+		});
+
+		filteredResults.Each(func(i int, s *goquery.Selection){
+			fmt.Printf("\n");
+			fmt.Printf("%v. %v\n", i+1, s.Text());
+		});
+
+		i += 1;
+	}
+
 }
